bdb308cfc9bc71fef10169973d098fca772187f4
simozz <simo.zz.dev@gmail.com>
2025-12-19 14:56:26 +0100

    add VHDL templates

--- .vim/after/ftdetect/vhdl.vim
+++ .vim/after/ftdetect/vhdl.vim
@@ -1,6 +1,6 @@
 augroup Vhdl
    autocmd!
-   autocmd BufReadPost,BufWritePost,BufNewFile,BufRead *.vhdl set filetype=vhdl
-   autocmd BufReadPost,BufWritePost,BufNewFile,BufRead *.vhdl if line('$') == 1 && getline(1) == '' | call InsTxtFromFile($HOME.'/.vim/templates/vhdl/vhdltemplate.vhdl') | call AdjTemplate() | %s/\/\//--/g | endif
+   autocmd BufReadPost,BufWritePost,BufNewFile,BufRead *.vhdl,*.vht set filetype=vhdl
+   autocmd BufReadPost,BufWritePost,BufNewFile,BufRead *.vhdl,*.vht if line('$') == 1 && getline(1) == '' | call InsTxtFromFile($HOME.'/.vim/templates/header/header.txt', line(1)) | if stridx(expand('%:t'), 'tb') != -1 | call InsTxtFromFile($HOME.'/.vim/templates/vhdl/vhdltbtemplate.vhdl', line('$')) | else | call InsTxtFromFile($HOME.'/.vim/templates/vhdl/vhdltemplate.vhdl', line('$')) | endif | call InsTxtFromFile($HOME.'/.vim/templates/header/footer.txt', line('$')) | call AdjTemplate() | exec ":%s/\\/\\//--/g" | endif
    autocmd FileType vhdl call SetIndentMarks() | call AddFtDict()
 augroup END
--- .vim/templates/vhdl/vhdltemplate.vhdl
+++ .vim/templates/vhdl/vhdltemplate.vhdl
@@ -7,30 +7,31 @@ use IEEE.numeric_std.all;
 entity $COMPONENT_NAME is
 ------------------------------- I/O declaration --------------------------------
 port ( 
-   Clk      : in  std_logic;
-   Reset_n  : in  std_logic;
-   Din      : in  std_logic_vector(7 downto 0);
-   Dout     : out std_logic_vector(2 downto 0) 
+    SClk      : in  std_logic;
+    SyncRstIn : in  std_logic;
+    SyncRstIn : in  std_logic;
+    Din       : in  std_logic_vector(7 downto 0);
+    Dout      : out std_logic_vector(2 downto 0)
 );
 end entity $COMPONENT_NAME;
 
 --------------------------------- Architecture ---------------------------------
 architecture RTL of $COMPONENT_NAME is
 begin
-   process (Clk)
-      variable R : unsigned (Dout'range);
-      begin
-      R := (others=>'0');
+    process (Clk)
+        variable R : unsigned (Dout'range);
+        begin
+            R := (others=>'0');
+        
+            if Clk'event and Clk = '1' then
+            for i in Din'range loop
+                if Din(i)='1' then
+                    R := R or to_unsigned(i, R'length);
+                end if;
+            end loop;
+        end if;
       
-      if Clk'event and Clk = '1' then
-         for i in Din'range loop
-            if Din(i)='1' then
-               R := R or to_unsigned(i,R'length);
-            end if;
-         end loop;
-      end if;
-      
-      Dout <= std_logic_vector(R);
-   end process;
+        Dout <= std_logic_vector(R);
+    end process;
 
 end architecture RTL;
